75e459b1d48c4f0f9cea83660cae8a5552c2dd9a
silver_anth <silver0anth@gmail.com>
2024-11-26 03:33:27 +0000

    try fix req again, wish I didn't need to do a commit every time but this crate doesn't compile on windows anymore

--- pubsub-client/src/pubsub_client.rs
+++ pubsub-client/src/pubsub_client.rs
@@ -309,14 +309,14 @@ fn connect_with_retry(
     headers: Vec<[String;2]>
 ) -> Result<WebSocket<MaybeTlsStream<TcpStream>>, tungstenite::Error> {
     let mut connection_retries = 5;
-    let HEADERS_CP: Vec<[String;2]> = headers.clone();
+    let headers_cp: Vec<[String;2]> = headers.clone();
     loop {
         let mut req = tungstenite::http::Request::builder()
             .uri(url.as_str());
 
         let mut n = 0;
-        while n < HEADERS_CP.len() {
-            req = req.header(HEADERS_CP[n][0].as_str(), HeaderValue::from_static(HEADERS_CP[n][1].as_str()));
+        while n < headers_cp.len() {
+            req = req.header(headers_cp[n][0].as_str(), headers_cp[n][1].as_str());
             n += 1;
         }
 
